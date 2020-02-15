373717ddb916c386ba9ff798e12aa7af0a66cd95
jalondono <juanlondono151776@hotmail.com>
2020-02-14 21:30:10 -0500

    * Added new feature - Show un h4 filter the chose items * Added style to handle the overflow

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
   const check = $('.popover INPUT');
-  const data_web = check.data().id;
+  const h4Text = $('.amenities h4');
   let data_dict = {};
+  let names = [];
 
     $('input[type="checkbox"]').click(function(){
+        h4Text.text('');
         if($(this).is(":checked")){
             data_dict[$(this).data().id] = $(this).data().name;
-            console.log(data_dict);
-        }
-        else if($(this).is(":not(:checked)")){
+
+        } else if($(this).is(":not(:checked)")){
              delete data_dict[($(this).data().id)];
-             console.log(data_dict);
         }
+        names = [];
+        for (let dataKey in  data_dict) {
+            names.push(data_dict[dataKey]);
+        }
+        h4Text.text(names.join(', '));
     });
 });
